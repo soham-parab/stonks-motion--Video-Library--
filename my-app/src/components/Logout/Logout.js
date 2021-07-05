6e9bf6106b6f68d12c504b32c7e9f37c514b35a2
soham-parab <57428835+soham-parab@users.noreply.github.com>
2021-07-05 19:29:50 +0530

    implemented logoutUser

--- my-app/src/components/Logout/Logout.js
+++ my-app/src/components/Logout/Logout.js
@@ -1,24 +1,8 @@
-import "./Logout.css";
 import React from "react";
 import { useAuth } from "../../contexts/authContext";
 
 export const Logout = () => {
   const { auth, setAuth } = useAuth();
 
-  function logoutHandler() {
-    setAuth(() => {
-      localStorage.removeItem("auth");
-    });
-  }
-
-  return (
-    <div>
-      <div>
-        <h1>Are you sure you want to log out?</h1>
-        <button className="btnPrimary" onClick={logoutHandler}>
-          Logout
-        </button>
-      </div>
-    </div>
-  );
+  return <div>Helo</div>;
 };
--- my-app/src/components/Nav/nav.js
+++ my-app/src/components/Nav/nav.js
@@ -1,26 +1,44 @@
 import "./nav.css";
 import { Link } from "react-router-dom";
 
+import { useAuth } from "../../contexts/authContext";
+
 export function Nav() {
-   return (
-      <div>
-         <div className="nav-header">
-            <p className="para">StonksMotion</p>
-         </div>
-         <div className="nav-div">
-            <ul className="nav">
-               <li classNameName="nav-item">
-                  <Link className="link-nav" to="/">
-                     Home
-                  </Link>
-               </li>
-               <li className="nav-item">
-                  <Link className="link-nav" to="/library">
-                     Library
-                  </Link>
-               </li>
-            </ul>
-         </div>
+  const { auth, setAuth } = useAuth();
+  function logoutHandler() {
+    setAuth(() => {
+      localStorage.removeItem("auth");
+    });
+  }
+
+  return (
+    <div>
+      <div className="nav-header">
+        <p className="para">StonksMotion</p>
       </div>
-   );
+      <div className="nav-div">
+        <ul className="nav">
+          <li classNameName="nav-item">
+            <Link className="link-nav" to="/">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="link-nav" to="/library">
+              Library
+            </Link>
+            {!auth ? (
+              <Link className="link-nav" to="/login">
+                Login
+              </Link>
+            ) : (
+              <Link className="link-nav" to="/" onClick={logoutHandler}>
+                Logout
+              </Link>
+            )}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
